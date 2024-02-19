ba37da512c6e166234a1ed679a69177e175d0fd2
Moody Rahman <moody.byte@gmail.com>
2024-02-19 17:12:48 -0500

    render posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,57 @@
     	export let data;
 
         const posts = data.data
-</script>
-<pre>
 
-    {JSON.stringify(posts, null, 2)}
-    
-</pre>
+        const generateDateString = (timestamp: string) => {
+            const z = new Date(timestamp)
+
+            return z.toLocaleString()
+
+        }
+</script>
+
 {#each posts as post}
+<div class="post">
     <a href={post.slug}>
-        {post.title}  <br>
+        {post.title}
     </a>
+    <small>
+        {generateDateString(post.createdAt)}
+    </small>
+    <small>
+        {#each post.tags as tag}
+            <span>
+                {tag}
+            </span>
+        {/each}
+    </small>
+    </div>
 {/each}
 
+
+<style>
+    .post {
+        margin-bottom: 1rem;
+        width: fit-content;
+        padding: 1rem;
+    }
+
+    small {
+        font-size: 1rem;
+        display:block;
+    }
+
+    small > span {
+        padding-left: .2rem;
+        padding-right: .2rem;
+        margin-left: .3rem;
+        margin-right: .3rem;
+
+
+        width: fit-content;
+
+        background-color: #34425c;
+        color: #cdcdc2;
+    }
+
+</style>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
+    import SvelteMarkdown from 'svelte-markdown'
     export let data;
 
-
-
-
+    const post = data.data?.find((x) => x.slug === data.slug)
+    const date = new Date(post?.createdAt!)
 </script>
 
+<h3>
+    {post?.title}
+</h3>
+<small>
+    published: {date.toString()}
+</small>
 
-hello there
+<p>
+    <SvelteMarkdown source={post?.content} />    
+</p>
 
-<pre>
 
-    {JSON.stringify(data, null, 2)}
-    
-</pre>
+
+
+<style>
+
+    h3 {
+        margin: 0;
+    }
+    small {
+        margin-top: 2%;
+        font-size: 1rem;
+    }
+
+    p {
+        margin-top: 5%;
+    }
+
+    .title {
+
+    }
+</style>
