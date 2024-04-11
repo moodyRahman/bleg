c1fe2b768f2446225461cf32afbc53ce8993685f
Moody Rahman <moody.byte@gmail.com>
2024-04-11 18:00:06 -0400

    updated xml format

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -8,6 +8,19 @@ interface Attributes {
 	tags: string[];
 }
 
+/**
+
+item>
+<title>Running Phoenix (elixir) Apps on Kubernetes</title>
+<link>https://jaronoff.com/posts/2021-03-02-elixir-kube/</link>
+<pubDate>Tue, 02 Mar 2021 00:00:00 +0000</pubDate>
+<guid>https://jaronoff.com/posts/2021-03-02-elixir-kube/</guid>
+<description>
+I&rsquo;m currently working on a side project for a friend (on the hush hush right now), and I&rsquo;ve had the pleasure of writing it in elixir using Phoenix. Having avoided touching react for a while, being able to write some pretty simple HTML on top of an elixir backend is a wonderful experience. Local development is simple, but how one goes about preparing and deploying their Phoenix app for Kubernetes is a whole other process.
+</description>
+</item>
+ */
+
 export async function GET({ fetch }) {
 	const req = await fetch('/api');
 	const raw_data = await req.json();
@@ -32,6 +45,8 @@ export async function GET({ fetch }) {
                 <title>${x.title}</title>
                 <link>https://bleg.moodyrahman.com/${x.slug}</link>
                 <description>${x.publishedAt}</description>
+				<guid>https://bleg.moodyrahman.com/${x.slug}</guid>
+				<description>${x.content.substring(0, 150)}...</description>
             </item>
         `
 					)
